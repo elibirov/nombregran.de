78c30e9028d9fcbc30ab3f4432fab138811babb2
Ilya Brovashov <59415530+elibirov@users.noreply.github.com>
2024-03-28 15:11:33 +0100

    First version

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Elias (Ilya) Brovashov")
+    content = """
+    Hi, my name is Elias! With extensive experience spanning 15 years  across IT, I emerge as a solutions-oriented professional in the areas of Cloud and AI. 
+    My deep-rooted analytical insight aligns seamlessly with a goal-centric approach, ensuring adaptability and resilience during transformative projects. 
+    Peers view me as a trusted consultant and solution architect, proficient in bridging gaps between stakeholders and translating visionary concepts into tangible solutions. 
+    I pride myself on establishing ambitious benchmarks, steering projects with unwavering integrity, and exhibiting expertise in the Cloud, AI, and Cybersecurity.
+"""
+    st.write(content)
