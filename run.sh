e5b0b38720805a84704bdc1af2efaa372d975ff5
Ilya Brovashov <59415530+elibirov@users.noreply.github.com>
2024-03-29 21:28:07 +0100

    run.sh server

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python -m streamlit run NombreGrande/main.py --server.port 8000 --server.address 0.0.0.0
+## python -m streamlit run main.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit run main.py --server.port 8000
